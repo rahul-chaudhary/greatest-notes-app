0b97f206fe1ba91146ccdaf7b2ed029fb09f13c1
unknown <bossrc64@outlook.com>
2022-03-04 13:26:33 +0530

    Alpha 2

--- lib/colors.dart
+++ lib/colors.dart
@@ -4,4 +4,5 @@ class MyColors {
   Color clrIndigo = const Color(0xff27283B);
   Color clrGreyLight = const Color(0xffEEEEEE);
   Color clrIndigoDark = const Color(0xff282A3E);
+  Color clrTeal = const Color(0xff00ADB5);
 }
--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,5 +1,4 @@
 import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'colors.dart';
 
@@ -11,11 +10,12 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
+  double scrnWth = 0.0;
   MyColors clr = MyColors();
 
   @override
   Widget build(BuildContext context) {
-    double scrnWth = MediaQuery.of(context).size.width; //screen width
+    scrnWth = MediaQuery.of(context).size.width; //Screen width
 
     return Scaffold(
       backgroundColor: clr.clrIndigo,
@@ -32,57 +32,100 @@ class _HomepageState extends State<Homepage> {
         toolbarHeight: 65.0,
         centerTitle: true,
       ),
-      body: SingleChildScrollView(
-          scrollDirection: Axis.vertical,
-          child: Column(
-            children: [
-              Row(
+      body: Column(
+        children: [
+          SingleChildScrollView(
+              scrollDirection: Axis.vertical,
+              child: Column(
                 children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: GestureDetector(
-                      child: SizedBox(
-                        width: scrnWth / 2.2,
-                        height: 150,
-                        child: Card(
-                          elevation: 5.0,
-                          color: clr.clrIndigo,
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Column(
-                              children: [
-                                FittedBox(
-                                  fit: BoxFit.fitWidth,
-                                  child: Text(
-                                    "Title",
-                                    style: TextStyle(
-                                      color: clr.clrGreyLight,
-                                      // fontSize: 25.0,
-                                      letterSpacing: 1.0,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.only(top: 12.0),
-                                  child: Text(
-                                      "This is  an example text and I am making a note app which is note taker.",
-                                      style: TextStyle(
-                                        fontSize: 12.0,
-                                        color: clr.clrGreyLight,
-                                      )),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
+                  Row(
+                    children: [
+                      pCard(),
+                      pCard(),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      pCard(),
+                      pCard(),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      pCard(),
+                      pCard(),
+                    ],
+                  ),
+                ],
+              )),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 5.0, horizontal: 8.0),
+            child: SizedBox(
+              width: scrnWth / 1.5,
+              height: 50.0,
+              child: ElevatedButton(
+                child: Text(
+                  "Add note",
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(clr.clrTeal),
+                  elevation: MaterialStateProperty.all<double>(5.0),
+                ),
+                onPressed: () {},
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Padding pCard() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: GestureDetector(
+        child: SizedBox(
+          width: scrnWth / 2.2,
+          height: 150,
+          child: Card(
+            elevation: 5.0,
+            color: clr.clrIndigo,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  FittedBox(
+                    fit: BoxFit.fitWidth,
+                    child: Text(
+                      "Title",
+                      style: TextStyle(
+                        color: clr.clrGreyLight,
+                        // fontSize: 25.0,
+                        letterSpacing: 1.0,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
                   ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 12.0),
+                    child: Text(
+                        "This is  an example text and I am making a note app which is note taker.",
+                        style: TextStyle(
+                          fontSize: 12.0,
+                          color: clr.clrGreyLight,
+                        )),
+                  ),
                 ],
               ),
-            ],
-          )),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
